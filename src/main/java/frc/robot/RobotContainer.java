0e76fdfdf83ce5af41d0676f7cbb5af133e6ab07
Sujay Jukalkar <sujukalkar@gmail.com>
2022-11-03 20:12:15 -0400

    configured DriveWithJoysticksCommand as the default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.commands.DriveWithJoysticksCommand;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +19,19 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final DrivetrainSubsystem swerve = new DrivetrainSubsystem();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+  }
+
+   private void configDefaultCommands(){
+    CommandScheduler scheduler = CommandScheduler.getInstance();
+
+    DriveWithJoysticksCommand defaultCommand = new DriveWithJoysticksCommand(swerve);
+    scheduler.setDefaultCommand(this.swerve, defaultCommand);
   }
 
   /**
@@ -43,6 +49,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
